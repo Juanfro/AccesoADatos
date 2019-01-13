5e6582b02b4114650f7df2e3999442a15412f21c
Juanfro <juanfropro@gmail.com>
2019-01-13 20:34:24 +0100

    Escribir a fichero binario

--- xmlFolder/listadoLibros/ListadoLibros.java
+++ xmlFolder/listadoLibros/ListadoLibros.java
@@ -1,7 +1,12 @@
 package listadoLibros;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -50,12 +55,16 @@ class ListadoLibros {
 
 	String fichero = "bookstore.xml";
 	String schemaName = "bookstore.xsd";
+	String ficheroBinario = "libros.dat";
 
 	Document document;
 
 	public static void main(String[] args) {
 
 		ListadoLibros listadoLibros = new ListadoLibros();
+
+		// Inicializar
+		listadoLibros.init();
 
 		// Listado de libros
 		listadoLibros.listado();
@@ -75,77 +84,75 @@ class ListadoLibros {
 			System.out.println("No valida - IllegalArgumentException");
 		}
 
+		listadoLibros.crearLibros();
+
 	}
 
-	void listado() {
+	void init() {
+
+		System.out.println("Inicializando\n\n");
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
-
 			document = builder.parse(fichero);
 			document.getDocumentElement().normalize();
-
-			// Element rootElement = document.getDocumentElement();
-
-			// NodeList nodes = rootElement.getChildNodes();
-
-			System.out.print("Elemento raiz: ");
-
-			System.out.println(document.getDocumentElement().getNodeName() + "\n");
-
-			// LISTA de LIBROS
-
-			NodeList nodes = document.getElementsByTagName("book");
-
-			for (int i = 0; i < nodes.getLength(); i++) {
-				Node nNode = nodes.item(i);
-				System.out.println("LIBRO " + (i + 1));
-
-				// System.out.println(nNode.getNodeName());
-				Element elementLibro = (Element) nNode;
-
-				// ID
-				NamedNodeMap atributos = nNode.getAttributes();				
-				
-				System.out.println("ID: " + atributos.item(0).getTextContent());
-
-				System.out.println("Titulo: " + elementLibro.getElementsByTagName("title").item(0).getTextContent());
-
-				System.out.println("Autor: " + elementLibro.getElementsByTagName("author").item(0).getTextContent());
-
-				NodeList genero = elementLibro.getElementsByTagName("genre");
-				if (genero.getLength() > 0) {
-					System.out
-							.println("Género: " + elementLibro.getElementsByTagName("genre").item(0).getTextContent());
-				}
-
-				System.out.println("Precio: " + elementLibro.getElementsByTagName("price").item(0).getTextContent());
-
-				NodeList fecha = elementLibro.getElementsByTagName("publish_date");
-				if (fecha.getLength() > 0) {
-					System.out.println("Fecha publicación: " + fecha.item(0).getTextContent());
-				}
-
-				NodeList description = elementLibro.getElementsByTagName("description");
-				if (description.getLength() > 0) {
-					System.out.println("Descripción: " + description.item(0).getTextContent());
-
-				}
-
-				System.out.println();
-
-			}
-
-			// System.out.println("FIN");
-
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	void listado() {
+
+		System.out.println("\nLISTADO DE LIBROS\n");
+
+		System.out.print("Elemento raiz: ");
+
+		System.out.println(document.getDocumentElement().getNodeName() + "\n");
+
+		// LISTA de LIBROS
+
+		NodeList nodes = document.getElementsByTagName("book");
+
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node nNode = nodes.item(i);
+			System.out.println("LIBRO " + (i + 1));
+
+			// System.out.println(nNode.getNodeName());
+			Element elementLibro = (Element) nNode;
+
+			// ID
+			NamedNodeMap atributos = nNode.getAttributes();
+
+			System.out.println("ID: " + atributos.item(0).getTextContent());
+
+			System.out.println("Titulo: " + elementLibro.getElementsByTagName("title").item(0).getTextContent());
+
+			System.out.println("Autor: " + elementLibro.getElementsByTagName("author").item(0).getTextContent());
+
+			NodeList genero = elementLibro.getElementsByTagName("genre");
+			if (genero.getLength() > 0) {
+				System.out.println("Género: " + elementLibro.getElementsByTagName("genre").item(0).getTextContent());
+			}
+
+			System.out.println("Precio: " + elementLibro.getElementsByTagName("price").item(0).getTextContent());
+
+			NodeList fecha = elementLibro.getElementsByTagName("publish_date");
+			if (fecha.getLength() > 0) {
+				System.out.println("Fecha publicación: " + fecha.item(0).getTextContent());
+			}
+
+			NodeList description = elementLibro.getElementsByTagName("description");
+			if (description.getLength() > 0) {
+				System.out.println("Descripción: " + description.item(0).getTextContent());
+			}
+
+			System.out.println();
+
 		}
 	}
 
@@ -164,6 +171,97 @@ class ListadoLibros {
 
 		validator.validate(new DOMSource(document));
 
+	}
+
+	/**
+	 * Utiliza la información de los libros (Books) en XML para crear objetos de la
+	 * clase Book y escribirlos en un fichero binario
+	 */
+	void crearLibros() {
+
+		String id = null;
+		String autor = null;
+		String titulo;
+		String genero;
+		float precio;
+		LocalDate fechaPublicacion;
+		String descripcion;
+
+		Libro unLibro;
+
+		List<Libro> libros = new ArrayList<Libro>();
+
+		System.out.println("\nEscribir datos en el fichero \"libros.dat\"");
+
+		NodeList nodes = document.getElementsByTagName("book");
+
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node nNode = nodes.item(i);
+			System.out.println("Leyendo libro" + (i + 1));
+			Element elementoLibro = (Element) nNode;
+
+			NamedNodeMap atributos = nNode.getAttributes();
+
+			if (atributos.getLength() > 0) {
+				id = atributos.getNamedItem("id").getTextContent();
+			}
+			// id = atributos.item(0).getTextContent();
+
+			NodeList nodoAutor = elementoLibro.getElementsByTagName("Autor");
+			if (nodoAutor.getLength() > 0) {
+				autor = nodoAutor.item(0).getTextContent();
+			} // autor =
+				// elementoLibro.getElementsByTagName("author").item(0).getTextContent();
+
+			NodeList nodoTitulo = elementoLibro.getElementsByTagName("title");
+			titulo = (nodoTitulo.getLength() > 0) ? nodoTitulo.item(0).getTextContent() : null;
+			// titulo =
+			// elementoLibro.getElementsByTagName("titulo").item(0).getTextContent();
+
+			NodeList nodoGenero = elementoLibro.getElementsByTagName("genre");
+			genero = (nodoGenero.getLength() > 0) ? nodoGenero.item(0).getTextContent() : null;
+			// genero =
+			// elementoLibro.getElementsByTagName("genre").item(0).getTextContent();
+
+			NodeList nodoPrecio = elementoLibro.getElementsByTagName("price");
+			precio = (nodoPrecio.getLength() > 0) ? Float.parseFloat(nodoPrecio.item(0).getTextContent()) : null;
+			// precio =
+			// Float.parseFloat(elementoLibro.getElementsByTagName("price").item(0).getTextContent());
+
+			NodeList nodoFechaPublicacion = elementoLibro.getElementsByTagName("publish_date");
+			fechaPublicacion = (nodoFechaPublicacion.getLength() > 0)
+					? LocalDate.parse(nodoFechaPublicacion.item(0).getTextContent())
+					: null;
+
+			// fechaPublicacion =
+			// LocalDate.parse(elementoLibro.getElementsByTagName("publish_date").item(0).getTextContent());
+
+			NodeList nodoDescripcion = elementoLibro.getElementsByTagName("description");
+			descripcion = (nodoDescripcion.getLength() > 0) ? nodoDescripcion.item(0).getTextContent() : null;
+			// descripcion =
+			// elementoLibro.getElementsByTagName("description").item(0).getTextContent();
+
+			unLibro = new Libro(id, autor, titulo, genero, precio, fechaPublicacion, descripcion);
+
+			libros.add(unLibro);
+
+			System.out.println("Objeto Libro " + (i + 1) + " creado");
+
+		}
+
+		escribirFichero(libros);
+
+	}
+
+	void escribirFichero(List<Libro> listaLibros) {
+		try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(ficheroBinario))) {
+			for (Libro libro : listaLibros) {
+				os.writeObject(libro);
+			}
+			os.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
