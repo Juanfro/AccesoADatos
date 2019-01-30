3ade6e24d3af45f85933377b7919b0e317af534d
Juanfro <juanfropro@gmail.com>
2019-01-30 19:46:39 +0100

    Exist-db

---
+++ xmlFolder/seleccionEXISTDB/SeleccionMain.java
@@ -0,0 +1,23 @@
+package seleccionEXISTDB;
+
+/**
+ * <h1>Seleccion</h1> Escribe un programa Java que ayude a Luis Enrique a
+ * conformar su lista de seleccionados para el siguiente partido del combinado
+ * nacional. <br>
+ * El programa permitirá al usuario listar, añadir y borrar jugadores de la
+ * lista de elegidos. <br>
+ * Cuando la aplicación se inicia lee la lista de jugadores de un fichero
+ * binario con la lista de objetos serializados, llamado jugadores.dat, tal y
+ * como se quedó al final de la ejecución anterior.<br>
+ * La lista de objetos se carga en memoria y se aplican las operaciones según
+ * las va solicitando el usuario.<br>
+ * Cuando el usuario decide cerrar el programa la lista de jugadores se vuelve a
+ * serializar y se vuelcan de nuevo al fichero.<br>
+ * Es importante tener en cuenta que solamente se lee y se graban los datos al
+ * fichero de jugadores cuando se inicia y finaliza la ejecución del programa.
+ * @author Juan Antonio Rodríguez
+ *
+ */
+class SeleccionMain {
+
+}
