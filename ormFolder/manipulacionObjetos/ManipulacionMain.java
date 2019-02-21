33571d8360e4bdd67f1033f568a5d4041750271e
Juanfro <juanfropro@gmail.com>
2019-02-21 11:25:02 +0100

    Entidades mapeadas generadas

--- ormFolder/manipulacionObjetos/ManipulacionMain.java
+++ ormFolder/manipulacionObjetos/ManipulacionMain.java
@@ -1,13 +1,16 @@
 package manipulacionObjetos;
 
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
 import com.mysql.cj.xdevapi.SessionFactory;
 
 public class ManipulacionMain {
 
 	public static void main(String[] args) {
 
-		SessionFactory sf = new Configuration().configure().buildSessionFactory();
-		Session session = sf.openSession();
+		SessionFactory sf = (SessionFactory) new Configuration().configure().buildSessionFactory();
+		Session session = ((org.hibernate.SessionFactory) sf).openSession();
 
 	}
 
